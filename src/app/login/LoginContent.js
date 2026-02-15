b85f1023d09460c1418e32e9de061f60b6f61856
venkatyashwanth <mudiliyashwanth@gmail.com>
2026-02-15 11:40:00 +0530

    added suspense error to prevent pre-rendering issues

---
+++ src/app/login/LoginContent.js
@@ -0,0 +1,86 @@
+"use client";
+import { useEffect, useState } from "react";
+import styles from "./page.module.scss";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter, useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import { auth } from "@/lib/firebase";
+
+export default function LoginContent() {
+    const authState = useSelector((state) => state.auth);
+    // const user = useSelector((state) => state.auth.user);
+    // const authLoading = useSelector((state) => state.auth.loading);
+    const user = authState?.user;
+    const authLoading = authState?.loading;
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const redirect = searchParams.get("redirect") || "/";
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const [loading,setLoading] = useState(false);
+    const [mounted,setMounted] = useState(false);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setError("");
+        setLoading(true);
+
+        try{
+            await signInWithEmailAndPassword(auth,email,password);
+            router.replace(redirect);
+        } 
+        catch(err){
+            console.error("Login Error: ",err);
+            setError(err.message);
+        }
+
+        setLoading(false);
+    };
+
+    useEffect(() =>{
+        setMounted(true);
+    },[])
+
+    useEffect(() => {
+        if(!authLoading && user){
+            router.replace("/");
+        }
+    },[user,authLoading,router])
+
+    if (!mounted) return null;
+    // if(typeof window === "undefined") return null;
+    return (
+        <div className={styles.wrapper}>
+            <form className={styles.form} onSubmit={handleLogin}>
+                <h1>Login</h1>
+                <input
+                    type="email"
+                    placeholder="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+
+                {error && <p className={styles.error}>{error}</p>}
+
+                <button type="submit" disabled={loading}>
+                    {loading? "Signing in..." : "Login"}
+                </button>
+
+                <div className={styles.redirect}>
+                    <span>Don't have an account?</span>
+                    <Link href="/register">Create One</Link>
+                </div>
+            </form>
+        </div>
+    )
+}
